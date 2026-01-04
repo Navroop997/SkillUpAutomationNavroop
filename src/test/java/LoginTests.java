7232a0a0d6ab3ee71d08fe3e0464347460475752
Navroop997 <singhnavroop1@gmail.com>
2026-01-04 14:23:29 -0800

    Update LoginTests.java
    update_demo

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -14,7 +14,7 @@ public class LoginTests extends BaseTest {
 
     @Test
     public void loginEmptyEmailPassword() {
-
+//demo
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
